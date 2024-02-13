2f0d7ba58d4724c6e93d930dd6761166be2faa17
masondprice <mason.price@students.fhu.edu>
2024-02-13 15:27:27 -0600

    Initial Commit

--- cat.py
+++ cat.py
@@ -1,8 +1,23 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self):
+        self.name = "Unknown"
+        self.age = 0
 
+    def speak(self):
+        print("Meow")
 
 #Create objects here
 #These should NOT be indented inside the class
 
+Stella = Cat()
+Stella.name = "Stella"
+Stella.age = 7
+
+Garfield = Cat()
+Garfield.name = "Garfield"
+Garfield.age = 50
+
+Stella.speak()
+
+Garfield.speak()
